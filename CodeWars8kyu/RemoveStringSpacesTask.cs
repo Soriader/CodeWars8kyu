fae775796629de8daaca2bdc1e0f8aed4405f607
NewLavoisier <kacperjwawrzyniak@gmail.com>
2024-03-14 17:10:01 +0100

    [~] Task: Remove String Spaces

---
+++ CodeWars8kyu/RemoveStringSpacesTask.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeWars8kyu
+{
+    public class RemoveStringSpacesTask
+    {
+        public static string NoSpace(string input)
+        {
+            var spaceDestroyer = input.Replace(" ", "");
+            return spaceDestroyer;
+        }
+    }
+}
+//https://www.codewars.com/kata/57eae20f5500ad98e50002c5/train/csharp
--- CodeWarsTest/UnitTest1.cs
+++ CodeWarsTest/UnitTest1.cs
@@ -12,6 +12,14 @@ namespace CodeWarsTest
         public class KataTest
         {
             [Test]
+            public void RemoveStringSpacesTest()
+            {
+                Assert.AreEqual("8j8mBliB8gimjB8B8jlB", RemoveStringSpacesTask.NoSpace("8 j 8   mBliB8g  imjB8B8  jl  B"));
+                Assert.AreEqual("88Bifk8hB8BB8BBBB888chl8BhBfd", RemoveStringSpacesTask.NoSpace("8 8 Bi fk8h B 8 BB8B B B  B888 c hl8 BhB fd"));
+                Assert.AreEqual("8aaaaaddddr", RemoveStringSpacesTask.NoSpace("8aaaaa dddd r     "));
+            }
+
+            [Test]
             public void GradeBookTest()
             {
                 Assert.AreEqual('A', GradeBookTask.GetGrade(95, 90, 93));

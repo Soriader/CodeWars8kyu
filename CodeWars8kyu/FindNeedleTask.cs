d925f22413b373d00b81f2db74967c65b9c017a7
NewLavoisier <kacperjwawrzyniak@gmail.com>
2024-03-16 19:17:06 +0100

    [~] Task: A Needle in the Haystack

---
+++ CodeWars8kyu/FindNeedleTask.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeWars8kyu
+{
+    public class FindNeedleTask
+    {
+        public static string FindNeedle(object[] haystack)
+        {
+            for (int i = 0; i < haystack.Length; i++)
+            {
+                if(haystack[i] != null && haystack[i].ToString().ToLower() == "needle")
+                {
+                    return $"found the needle at position {i}";
+                }
+            }
+
+            return "";
+        }
+    }
+}
+//https://www.codewars.com/kata/56676e8fabd2d1ff3000000c/train/csharp
--- CodeWarsTest/UnitTest1.cs
+++ CodeWarsTest/UnitTest1.cs
@@ -12,6 +12,14 @@ namespace CodeWarsTest
         public class KataTest
         {
             [Test]
+            public void FindNeedleTest()
+            {
+                Assert.AreEqual("found the needle at position 3", FindNeedleTask.FindNeedle(new object[] { '3', "123124234", null, "needle", "world", "hay", 2, '3', true, false }));
+                Assert.AreEqual("found the needle at position 5", FindNeedleTask.FindNeedle(new object[] { "283497238987234", "a dog", "a cat", "some random junk", "a piece of hay", "needle", "something somebody lost a while ago" }));
+                Assert.AreEqual("found the needle at position 30", FindNeedleTask.FindNeedle(new object[] { 1, 2, 3, 4, 5, 6, 7, 8, 8, 7, 5, 4, 3, 4, 5, 6, 67, 5, 5, 3, 3, 4, 2, 34, 234, 23, 4, 234, 324, 324, "needle", 1, 2, 3, 4, 5, 5, 6, 5, 4, 32, 3, 45, 54 }));
+            }
+
+            [Test]
             public void FakeBinaryTest()
             {
                 Assert.AreEqual("01011110001100111", FakeBinaryTask.FakeBin("45385593107843568"));

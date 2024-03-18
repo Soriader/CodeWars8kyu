2fe0cf3a2d5edb8421dec02558765aef9e4b9fb2
NewLavoisier <kacperjwawrzyniak@gmail.com>
2024-03-18 13:05:42 +0100

    [~] Task: Beginner - Lost Without a Map

---
+++ CodeWars8kyu/LostWithoutAMapTask.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeWars8kyu
+{
+    public class LostWithoutAMapTask
+    {
+        public static int[] Maps(int[] x)
+        {
+
+            for (int i = 0; i < x.Length; i++)
+            {
+                 x[i] *= 2;
+            }
+
+            return x;
+        }
+    }
+}
--- CodeWarsTest/UnitTest1.cs
+++ CodeWarsTest/UnitTest1.cs
@@ -14,6 +14,14 @@ namespace CodeWarsTest
             class Test
             {
                 [Test]
+                public void LostWithoutAMapTest()
+                {
+                    Assert.AreEqual(new[] { 2, 4, 6 }, LostWithoutAMapTask.Maps(new[] { 1, 2, 3 }));
+                    Assert.AreEqual(new[] { 8, 2, 2, 2, 8 }, LostWithoutAMapTask.Maps(new[] { 4, 1, 1, 1, 4 }));
+                    Assert.AreEqual(new[] { 4, 4, 4, 4, 4, 4 }, LostWithoutAMapTask.Maps(new[] { 2, 2, 2, 2, 2, 2 }));
+                }
+
+                [Test]
                 public void InvertValuesTest()
                 {
                     Assert.AreEqual(new int[] { -1, -2, -3, -4, -5 }, InvertValuesTask.InvertValues(new int[] { 1, 2, 3, 4, 5 }));

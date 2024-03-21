0cc240b31d0a247fbef2e109640449341a2dce94
NewLavoisier <kacperjwawrzyniak@gmail.com>
2024-03-21 19:00:26 +0100

    [~] Task: Lario and Muigi Pipe Problem

---
+++ CodeWars8kyu/LarioAndMuigiTask.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeWars8kyu
+{
+    public class LarioAndMuigiTask
+    {
+        public static List<int> PipeFix(List<int> numbers)
+        {
+            int startIndex = numbers.Min();
+            int endIndex = numbers.Max();
+            List<int> result = new List<int>();
+
+            for (int i = startIndex; i <= endIndex; i++)
+            {
+                result.Add(i);
+            }
+
+            return result;
+
+        }
+    }
+}
+//https://www.codewars.com/kata/56b29582461215098d00000f/train/csharp
--- CodeWarsTest/UnitTest1.cs
+++ CodeWarsTest/UnitTest1.cs
@@ -14,6 +14,16 @@ namespace CodeWarsTest
             class Test
             {
                 [Test]
+                public void LarioAndMuigiTest()
+                {
+                    Assert.AreEqual(new List<int> { 2 }, LarioAndMuigiTask.PipeFix(new List<int> { 2 }));
+                    Assert.AreEqual(new List<int> { 1, 2, 3, 4, 5, 6, 7, 8, 9 }, LarioAndMuigiTask.PipeFix(new List<int> { 1, 2, 3, 5, 6, 8, 9 }));
+                    Assert.AreEqual(new List<int> { -1, 0, 1, 2, 3, 4 }, LarioAndMuigiTask.PipeFix(new List<int> { -1, 4 }));
+                    Assert.AreEqual(new List<int> { 6, 7, 8, 9 }, LarioAndMuigiTask.PipeFix(new List<int> { 6, 9 }));
+                    Assert.AreEqual(new List<int> { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12 }, LarioAndMuigiTask.PipeFix(new List<int> { 1, 2, 3, 12 }));
+                }
+
+                [Test]
                 public void DrinkAboutTest()
                 {
                     Assert.AreEqual("drink whisky", DrinkAboutTask.PeopleWithAgeDrink(22));

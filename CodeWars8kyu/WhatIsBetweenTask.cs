53a90804e4349db8bdfe1ac0e022e99abba22df4
NewLavoisier <kacperjwawrzyniak@gmail.com>
2024-04-01 17:09:10 +0200

    [~]Task: What is between?

---
+++ CodeWars8kyu/WhatIsBetweenTask.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeWars8kyu
+{
+    public class WhatIsBetweenTask
+    {
+        public static int[] Between(int a, int b)
+        {
+            int[] result = new int[b - a + 1]; 
+
+            for (int i = 0; i < result.Length; i++)
+            {
+                result[i] = a + i; 
+            }
+
+            return result;
+        }
+    }
+}
--- CodeWarsTest/UnitTest1.cs
+++ CodeWarsTest/UnitTest1.cs
@@ -14,6 +14,14 @@ namespace CodeWarsTest
         {
             class Test
             {
+                [Test]
+                public void WhatIsBetweenTest()
+                {
+                    Assert.AreEqual(new int[] { 0, 1, 2, 3 }, WhatIsBetweenTask.Between(0, 3));
+                    Assert.AreEqual(new int[] { -2, -1, 0, 1, 2 }, WhatIsBetweenTask.Between(-2, 2));
+                    Assert.AreEqual(new int[] { -10, -9, -8, -7, -6, -5, -4, -3, -2, -1, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 }, WhatIsBetweenTask.Between(-10, 10));
+                }
+
 
                 [Test]
                 public void PowersOfTwoTest()

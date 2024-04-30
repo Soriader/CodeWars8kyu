15d617639b60aa8d974837e950c00060becc1247
NewLavoisier <kacperjwawrzyniak@gmail.com>
2024-04-30 15:45:42 +0200

    [~] Task: Is there a vowel in there?

---
+++ CodeWars8kyu/IsThereAVowelInThereTask.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace CodeWars8kyu
+{
+    public class IsThereAVowelInThereTask
+    {
+        public static object[] IsVow(object[] a)
+        {
+            for (int i = 0; i < a.Length; i++)
+            {
+                switch (a[i])
+                {
+                    case 97:
+                        {
+                            a[i] = "a";
+                            break;
+                        }
+                    case 101:
+                        {
+                            a[i] = "e";
+                            break;
+                        }
+                    case 105:
+                        {
+                            a[i] = "i";
+                            break;
+                        }
+                    case 111:
+                        {
+                            a[i] = "o";
+                            break;
+                        }
+                    case 117:
+                        {
+                            a[i] = "u";
+                            break;
+                        }
+                }
+
+            }
+
+            return a;
+
+        }
+    }
+}
--- CodeWarsTest/UnitTest1.cs
+++ CodeWarsTest/UnitTest1.cs
@@ -14,7 +14,13 @@ namespace CodeWarsTest
         {
             class Test
             {
-				[Test]
+                [Test]
+                public void IsThereAVowelInThereTest()
+                {
+                    Assert.AreEqual(new object[] { 118, "u", 120, 121, "u", 98, 122, "a", 120, 106, 104, 116, 113, 114, 113, 120, 106 }, IsThereAVowelInThereTask.IsVow(new object[] { 118, 117, 120, 121, 117, 98, 122, 97, 120, 106, 104, 116, 113, 114, 113, 120, 106 }));
+                    Assert.AreEqual(new object[] { "e", 121, 110, 113, 113, 103, 121, 121, "e", 107, 103 }, IsThereAVowelInThereTask.IsVow(new object[] { 101, 121, 110, 113, 113, 103, 121, 121, 101, 107, 103 }));
+                }
+                [Test]
 				public void SumOfMultiplesTest()
 				{
 					Assert.AreEqual(20, SumOfMultiplesTask.SumMul(2, 9), "n = 2, m = 9");

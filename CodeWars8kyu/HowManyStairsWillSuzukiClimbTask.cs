eed8ea72f0a58b2a5c3ddb79d503f682fcf83ef2
NewLavoisier <kacperjwawrzyniak@gmail.com>
2024-05-29 11:01:16 +0200

    [~] Task: How many stairs will Suzuki climb in 20 years?

---
+++ CodeWars8kyu/HowManyStairsWillSuzukiClimbTask.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeWars8kyu
+{
+    public class HowManyStairsWillSuzukiClimbTask
+    {
+        public static long StairsIn20(int[][] stairs)
+        {
+            long result = 0;
+
+            foreach (var stair in stairs)
+            {
+                result += stair.Aggregate((x, y) => x + y);
+            }
+
+            return result*20;
+        }
+    }
+}
+//https://www.codewars.com/kata/56fc55cd1f5a93d68a001d4e/train/csharp

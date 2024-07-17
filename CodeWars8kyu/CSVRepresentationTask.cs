a51d5e8883609ee712fe35c57ba8752f075197a3
NewLavoisier <kacperjwawrzyniak@gmail.com>
2024-07-17 19:15:22 +0200

    [~] Task: CSV representation of array

---
+++ CodeWars8kyu/CSVRepresentationTask.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeWars8kyu
+{
+    public class CSVRepresentationTask
+    {
+        public static string ToCsvText(int[][] array)
+        {
+            StringBuilder result = new StringBuilder();
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                for (int j = 0; j < array[i].Length; j++)
+                {
+                    result.Append(array[i][j]);
+                    if (j < array[i].Length - 1)
+                    {
+                        result.Append(",");
+                    }
+                }
+                if (i < array.Length - 1)
+                {
+                    result.Append("\n");
+                }
+            }
+
+            return result.ToString();
+        }
+    }
+}
--- CodeWarsTest/UnitTest1.cs
+++ CodeWarsTest/UnitTest1.cs
@@ -17,6 +17,20 @@ namespace CodeWarsTest
             class Test
             {
                 [Test]
+                public void SampleTest()
+                {
+                    Assert.AreEqual("0,1,2,3,45\n10,11,12,13,14\n20,21,22,23,24\n30,31,32,33,34",
+                        CSVRepresentationTask.ToCsvText(new[]
+                        {
+                new[] {0, 1, 2, 3, 45},
+                new[] {10, 11, 12, 13, 14},
+                new[] {20, 21, 22, 23, 24},
+                new[] {30, 31, 32, 33, 34}
+                        })
+                    );
+                }
+
+                    [Test]
                 public void GeometryBasicsTest()
                 {
                     DoTest(new Point(3, 3), new Point(3, 3), 0);

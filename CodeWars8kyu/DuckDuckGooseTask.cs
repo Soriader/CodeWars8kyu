5de34d25037c10685a26354b48b267d199bde77e
NewLavoisier <kacperjwawrzyniak@gmail.com>
2024-07-24 13:52:29 +0200

    [~] Task: Duck Duck Goose

---
+++ CodeWars8kyu/DuckDuckGooseTask.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeWars8kyu
+{
+	public class DuckDuckGooseTask
+	{
+		public string Name { get; set; }
+
+		public DuckDuckGooseTask(string name)
+		{
+			this.Name = name;
+		}
+
+		public static string DuckDuckGoose(DuckDuckGooseTask[] players, int goose)
+		{
+			int index = (goose - 1) % players.Length;
+			return players[index].Name;
+		}
+	}
+}
+//https://www.codewars.com/kata/582e0e592029ea10530009ce/train/csharp
--- CodeWarsTest/UnitTest1.cs
+++ CodeWarsTest/UnitTest1.cs
@@ -16,7 +16,31 @@ namespace CodeWarsTest
         {
             class Test
             {
-				[Test]
+
+
+                [TestFixture]
+                public class DuckDuckGooseTests
+                {
+                    [TestCase(1, "a")]
+                    [TestCase(3, "c")]
+                    [TestCase(10, "z")]
+                    [TestCase(20, "z")]
+                    [TestCase(30, "z")]
+                    [TestCase(18, "g")]
+                    [TestCase(28, "g")]
+                    [TestCase(12, "b")]
+                    [TestCase(2, "b")]
+                    [TestCase(7, "f")]
+                    public void DuckDuckGooseTest(int goose, string expectedName)
+                    {
+                        var exampleNames = new string[] { "a", "b", "c", "d", "c", "e", "f", "g", "h", "z" };
+                        var players = exampleNames.Select(name => new DuckDuckGooseTask(name)).ToArray();
+                        string result = DuckDuckGooseTask.DuckDuckGoose(players, goose);
+                        Assert.AreEqual(expectedName, result);
+                    }
+                }
+
+					[Test]
 				public static void OnlineRPGTest()
 				{
 					Assert.AreEqual(OnlineRPGTask.PlayerRankUp(64), false);

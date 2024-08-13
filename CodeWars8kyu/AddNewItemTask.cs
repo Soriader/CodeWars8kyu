65c73d8c38c7fc0323604cd72972079e6aa2cf93
NewLavoisier <kacperjwawrzyniak@gmail.com>
2024-08-13 07:33:56 +0200

    [~] Task: Add new item (collections are passed by reference)

---
+++ CodeWars8kyu/AddNewItemTask.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeWars8kyu
+{
+	public class AddNewItemTask
+	{
+		public static List<int> AddExtra(List<int> listOfNumbers)
+		{
+			List<int> list = new List<int>(listOfNumbers);
+
+			int test = 1;
+
+			list.Add(test);
+
+			return list;
+		}
+	}
+}
+//https://www.codewars.com/kata/566dc05f855b36a031000048/train/csharp
--- CodeWarsTest/UnitTest1.cs
+++ CodeWarsTest/UnitTest1.cs
@@ -17,6 +17,13 @@ namespace CodeWarsTest
             class Test
             {
 				[Test]
+				public void AddNewItemTest()
+				{
+					List<int> listOfNumbers = new List<int>() { 1, 2, 2, 2, 4, 3, 4, 5, 6, 7 };
+					Assert.AreEqual((AddNewItemTask.AddExtra(listOfNumbers)).Count, listOfNumbers.Count + 1);
+				}
+
+				[Test]
 				public void GeometryBasicsCircleArea2DTest()
 				{
 
